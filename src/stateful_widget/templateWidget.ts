841f30d2c6814ce4fb84efa3ca441d071887f7c7
Denis Nascimento <fdenisnascimento@gmail.com>
2021-12-21 09:51:07 -0300

    initial commit

---
+++ src/stateful_widget/templateWidget.ts
@@ -0,0 +1,14 @@
+export default function getWidgetData(widgetName: string, fileName: string): string {
+  return `import 'package:flutter/material.dart';
+import './${fileName}_view.dart';
+
+class ${widgetName} extends StatefulWidget {
+  static const route = '/${widgetName}/';
+  const Home({Key? key}) : super(key: key);
+  
+  @override
+  ${widgetName}View createState() =>  ${widgetName}View();
+}
+  
+`;
+}
---
+++ src/stateful_widget/templateWidgetView.ts
@@ -0,0 +1,15 @@
+export default function getWidgetViewData(widgetName: string, fileName: string) {
+  return `import 'package:flutter/material.dart';
+import './${fileName}_view_model.dart';
+  
+class ${widgetName}View extends ${widgetName}ViewModel {
+    
+  @override
+  Widget build(BuildContext context) {    
+    // Replace this with your build function
+    return const Text('Just a placeholder');
+  }
+}
+
+`;
+}
